f3d458f786059ba985a34a13cf9366f821c96e11
Lukasz Mentel <lmmentel@gmail.com>
2016-11-21 17:26:53 +0100

    updated main with path specification

--- app/main.py
+++ app/main.py
@@ -2,10 +2,15 @@
 
 '''Entry point to all things to avoid circular imports.'''
 
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+
 from mendeleevapp import app
 from views import *
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
